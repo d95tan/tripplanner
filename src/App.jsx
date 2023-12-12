969fbc52486eadd06849a5f81b8691aae70968c3
Dan Desktop <d95tan@gmail.com>
2023-12-12 17:17:13 +0800

    checked apis, add debug and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "/src/pages/LandingPage/LandingPage";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [state, setState] = useState("");
+
+  const url = "http://api.aviationstack.com/v1/flights?access_key=792d16340973bf877385cb86feb9b75b&flight_iata=tr3"
+
+    useEffect(() => {
+      async function apiCall() {
+        const response = await fetch(url);
+        const json = await response.json();
+        setState(json);
+        console.log(json);
+      }
+      apiCall();
+    },[]);
+
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,6 @@
+export default function LandingPage() {
+    return <>
+        <h1>Welcome to TripPlanner</h1>
+        <h3>Plan your next trip with us!</h3>
+    </>
+}
