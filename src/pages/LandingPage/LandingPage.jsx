14014911f639067b810b508f92d9c4b5bca3a9ba
Dan Desktop <d95tan@gmail.com>
2023-12-30 18:48:59 +0800

    Feat: landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,6 @@
 import NavbarOut from "../../Components/NavbarOut"
+import Button from "react-bootstrap/esm/Button"
+import { NavLink } from "react-router-dom"
 
 export default function LandingPage() {
     return <>
@@ -6,8 +8,10 @@ export default function LandingPage() {
         <div className="landing-page-div gradient-background">
             <div className="landing-page-text">
                 <h1>Welcome to WanderWell!</h1>
-                <h5>Plan Your Perfect Trip with Ease</h5>
-                <p></p>
+                <h5>Plan Your Perfect Trip with Ease</h5><br />
+                <Button variant="primary" as={NavLink} to="/signup">
+                    Sign Up for free
+                </Button>
             </div>
             <img className="landing-page-first-img" src="Isometric Planning Page.png" />
         </div>
@@ -18,6 +22,14 @@ export default function LandingPage() {
                 <h2>Create personalised itineraries <br />tailored to your travel needs.</h2>
                 <h6>Add and Edit Events and Activities with ease.</h6>
             </div>
+        </div>
+
+        <div className="landing-page-div">
+            <div className="landing-page-text">
+                <h2>Stay Informed with <br />the Latest Information</h2>
+                <h6>Access up-to-date weather forecasts so you can plan accordingly. <br /> Currency calculator coming soon!</h6>
+            </div>
+            <img className="landing-page-edit-img" src="Weather.png" />
         </div>
     </>
 }
